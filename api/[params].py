ff4c89dac485838be501557006b2329b3ec175cb
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-03-22 16:41:03 +0100

    added error codes for no grib data and missing waypoints

--- api/[params].py
+++ api/[params].py
@@ -16,6 +16,11 @@ def fetch_image(url, etag_src):
         return Response("ogimet gramet TimeOut", status=408)
     data = r.text
     if data:
+        if data.find('no grib data') >=0 :
+            return Response('no grib data', status=503) # wmo non reconnu
+        m = re.search(r'gramet_lee_rutind: Error, no se han encontrado datos de (.+)', data)
+        if m:
+            return Response(m.group(1), status=409) # wmo non reconnu
         m = re.search(r'<img src="([^"]+/gramet_[^"]+)"', data)
         if m:
             img_src = "{url.scheme}://{url.netloc}{path}".format(
