1b72d47695a8d1fb5e15845313c1d17099062fcd
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-04-03 19:06:00 +0200

    expose X-ofp2map-status

--- api/[params].py
+++ api/[params].py
@@ -66,7 +66,7 @@ def proxy_gramet(hini, tref, hfin, fl, wmo, name):
     # add CORS headers
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add("Access-Control-Allow-Headers", "X-Requested-With")
-    response.headers.add("Access-Control-Expose-Headers", "ETag, X-ETag")
+    response.headers.add("Access-Control-Expose-Headers", "ETag, X-ETag, X-ofp2map-status")
     if (response.status_code != 200):
         response.headers.add('X-ofp2map-status', response.status)
     return response
